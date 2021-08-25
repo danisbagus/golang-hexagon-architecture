6235c84240c4ac8532c27bc17d6febac45d6b747
danisbagus <danisbagussetiawan@gmail.com>
2021-08-25 16:13:36 +0700

    Add env file

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/danisbagus/golang-hexagon-architecture/internal/core/service"
 	v1 "github.com/danisbagus/golang-hexagon-architecture/internal/handler/v1"
 	"github.com/danisbagus/golang-hexagon-architecture/internal/repo"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	// multiplexer
 	router := mux.NewRouter()
 
@@ -31,6 +39,7 @@ func main() {
 	v1Router.HandleFunc("/products/{product_id:[0-9]+}", productHandlerV1.GetProductDetailV1).Methods(http.MethodGet).Name("GetProductDetail")
 
 	// starting server
-	fmt.Println("Starting the application")
-	log.Fatal(http.ListenAndServe("localhost:9000", router))
+	appPort := fmt.Sprintf("localhost:%v", os.Getenv("APP_PORT"))
+	fmt.Println("Starting the application :", appPort)
+	log.Fatal(http.ListenAndServe(appPort, router))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/danisbagus/golang-hexagon-architecture
 
 go 1.13
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
+)
